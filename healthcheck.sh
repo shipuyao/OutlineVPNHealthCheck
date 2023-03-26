150ebf4da6cfe8424093a144874d840199090798
shipuyao <127500560+shipuyao@users.noreply.github.com>
2023-03-26 20:40:41 +0800

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-  
+
 tenantId="144dfc9d-3e1f-4c03-8e42-27eef2458df5"
 spAppId="bdd24406-7234-4bea-b9e3-69a712cd9851"
+spSecret=${SP_SECRET}
 location="southeastasia"
 resourceGroupName="VPN"
 nicName="vpn1989"
 publicIpName="VPN1-ip"
 ipConfigName="ipconfig1"
 
+storageAccountName="sparknas"
+storageAccountKey=${STORAGE_ACCOUT_KEY}
+containerName="vpn-access-keys"
+blobName="VPN1-AccessKey.txt"
+
+vpnPassword=${VPN_PWD}
+
 date
 
-az login --service-principal -u $spAppId -p $SP_SECRET --tenant $tenantId
+az login --service-principal -u $spAppId -p $spSecret --tenant $tenantId
 
 if [ $? != 0 ]; then
   echo "az login failed"
@@ -22,7 +30,7 @@ fi
 pip=$(az vm show -d -g VPN -n VPN1 --query publicIps -o tsv)
 
 # Test Outline VPN server connection
-nc -nvzw5 $pip 27909
+nc -nvzw5 $pip 27908
 
 if [ $? == 0 ]; then
   echo "Outline server connection is health, PIP rotation skipped"
@@ -34,26 +42,39 @@ echo "Start PIP rotation"
 
 echo "Disassociate the public IP address from the NIC"
 az network nic ip-config update \
-  --resource-group $resourceGroupName \
-  --nic-name $nicName \
-  --name $ipConfigName \
-  --remove PublicIpAddress
+	--resource-group $resourceGroupName \
+	--nic-name $nicName \
+	--name $ipConfigName \
+	--remove PublicIpAddress
 
 echo "Delete the public IP"
 az network public-ip delete -g $resourceGroupName -n $publicIpName
 
 echo "Create a new public IP"
 az network public-ip create \
-    --resource-group VPN \
-    --name VPN1-ip \
-    --sku Standard \
-    --version IPv4 \
-    --location $location \
-    --zone 1 2 3
+	--resource-group $resourceGroupName \
+	--name $publicIpName \
+	--sku Standard \
+	--version IPv4 \
+	--location $location \
+	--zone 1 2 3
 
 echo "Update ipconfig with new public IP" 
 az network nic ip-config update \
-  --resource-group $resourceGroupName --nic-name $nicName \
+	--resource-group $resourceGroupName --nic-name $nicName \
   --name $ipConfigName --public-ip-address $publicIpName
+
+
+newIP=$(az vm show -d -g VPN -n VPN1 --query publicIps -o tsv)
+
+echo "ss://$vpnPassword@$newIP:27909/?outline=1" > ./VPNAccessKey
+
+echo "Rotate Outline access key with new public IP $newIP"
+az storage blob upload --account-name $storageAccountName \
+	--account-key $storageAccountKey \
+	--container-name $containerName \
+	--file ./VPNAccessKey \
+	--name $blobName \
+	--overwrite
 
 echo
