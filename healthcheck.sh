a9f316ea607eb682982102ee6317dad907fb3b82
shipuyao <127500560+shipuyao@users.noreply.github.com>
2023-03-27 12:18:13 +0800

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 
-. /etc/profile
-. ~/.bash_profile
-. ~/.bashrc
-
 tenantId="144dfc9d-3e1f-4c03-8e42-27eef2458df5"
 spAppId="bdd24406-7234-4bea-b9e3-69a712cd9851"
-spSecret=$SP_SECRET
 location="southeastasia"
 resourceGroupName="VPN"
+vmName="VPN1"
 nicName="vpn1989"
 publicIpName="VPN1-ip"
 ipConfigName="ipconfig1"
 
 storageAccountName="sparknas"
-storageAccountKey=$STORAGE_ACCOUNT_KEY
 containerName="vpn-access-keys"
 blobName="VPN1-AccessKey.txt"
 
-vpnPassword=$VPN_PWD
-
 date
 
-az login --service-principal -u $spAppId -p $spSecret --tenant $tenantId
+az login --service-principal -u $spAppId -p $SP_SECRET --tenant $tenantId
 
 if [ $? != 0 ]; then
   echo "az login failed"
@@ -31,10 +24,12 @@ if [ $? != 0 ]; then
 fi
 
 # Get VM public ip address
-pip=$(az vm show -d -g VPN -n VPN1 --query publicIps -o tsv)
+pip=$(az vm show -d -g $resourceGroupName -n $vmName --query publicIps -o tsv)
+
+echo "Start testing Outline server $pip:27909 connectivity"
 
 # Test Outline VPN server connection
-nc -nvzw5 $pip 27908
+nc -nvzw5 $pip 27909
 
 if [ $? == 0 ]; then
   echo "Outline server connection is health, PIP rotation skipped"
@@ -69,16 +64,17 @@ az network nic ip-config update \
   --name $ipConfigName --public-ip-address $publicIpName
 
 
-newIP=$(az vm show -d -g VPN -n VPN1 --query publicIps -o tsv)
+newIP=$(az vm show -d -g $resourceGroupName -n $vmName --query publicIps -o tsv)
 
-echo "ss://$vpnPassword@$newIP:27909/?outline=1" > ./VPNAccessKey
+echo "ss://$VPN_PWD@$newIP:27909/?outline=1" > ./VPNAccessKey
 
 echo "Rotate Outline access key with new public IP $newIP"
 az storage blob upload --account-name $storageAccountName \
-	--account-key $storageAccountKey \
+	--account-key $STORAGE_ACCOUNT_KEY \
 	--container-name $containerName \
 	--file ./VPNAccessKey \
 	--name $blobName \
 	--overwrite
 
+echo "Done"
 echo
