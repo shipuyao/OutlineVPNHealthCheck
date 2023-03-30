1365fc0cda6c2779e9ae6e14ef395503ca28fe04
Obiwan <127500560+shipuyao@users.noreply.github.com>
2023-03-30 06:42:03 +0000

    add entrypoint.sh

---
+++ v2/docker-entrypoint.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+set -e
+container=docker
+export container
+
+if [ $# -eq 0 ]; then
+	echo >&2 'ERROR: No command specified. You probably want to run `journalctl -f`, or maybe `bash`?'
+	exit 1
+fi
+
+if [ ! -t 0 ]; then
+	echo >&2 'ERROR: TTY needs to be enabled (`docker run -t ...`).'
+	exit 1
+fi
+
+env >/etc/docker-entrypoint-env
+
+cat >/etc/systemd/system/docker-entrypoint.target <<EOF
+[Unit]
+Description=the target for docker-entrypoint.service
+Requires=docker-entrypoint.service vpnhealthcheck.service systemd-logind.service systemd-user-sessions.service
+EOF
+
+quoted_args="$(printf " %q" "${@}")"
+echo "${quoted_args}" >/etc/docker-entrypoint-cmd
+
+cat >/etc/systemd/system/docker-entrypoint.service <<EOF
+[Unit]
+Description=docker-entrypoint.service
+
+[Service]
+ExecStart=/bin/bash -exc "source /etc/docker-entrypoint-cmd"
+# EXIT_STATUS is either an exit code integer or a signal name string, see systemd.exec(5)
+ExecStopPost=/bin/bash -ec "if echo \${EXIT_STATUS} | grep [A-Z] > /dev/null; then echo >&2 \"got signal \${EXIT_STATUS}\"; systemctl exit \$(( 128 + \$( kill -l \${EXIT_STATUS} ) )); else systemctl exit \${EXIT_STATUS}; fi"
+StandardInput=tty-force
+StandardOutput=inherit
+StandardError=inherit
+WorkingDirectory=$(pwd)
+EnvironmentFile=/etc/docker-entrypoint-env
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat >/etc/systemd/system/vpnhealthcheck.timer <<EOF
+# /etc/systemd/system/vpnhealthcheck.timer
+
+[Unit]
+Description=Run vpn health check every 5 minutes
+Requires=vpnhealthcheck.service
+
+[Timer]
+Unit=vpnhealthcheck.service
+OnActiveSec=5min
+OnUnitInactiveSec=5min
+
+[Install]
+WantedBy=timers.target
+EOF
+
+cat >/etc/systemd/system/vpnhealthcheck.service <<EOF
+# /etc/systemd/system/vpnhealthcheck.service
+
+[Unit]
+Description=Cronjob for vpnhealthcheck.service
+
+# This will auto-start the cron timer
+Wants=vpnhealthcheck.timer
+
+[Service]
+EnvironmentFile=/root/healthcheck-env
+Type=oneshot
+ExecStart=/bin/bash -c /root/healthcheck.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+systemctl mask systemd-firstboot.service systemd-udevd.service systemd-modules-load.service
+systemctl unmask systemd-logind
+systemctl enable docker-entrypoint.service vpnhealthcheck.service
+
+systemd=
+if [ -x /lib/systemd/systemd ]; then
+	systemd=/lib/systemd/systemd
+elif [ -x /usr/lib/systemd/systemd ]; then
+	systemd=/usr/lib/systemd/systemd
+elif [ -x /sbin/init ]; then
+	systemd=/sbin/init
+else
+	echo >&2 'ERROR: systemd is not installed'
+	exit 1
+fi
+systemd_args="--show-status=false --unit=docker-entrypoint.target"
+echo "$0: starting $systemd $systemd_args"
+exec $systemd $systemd_args
